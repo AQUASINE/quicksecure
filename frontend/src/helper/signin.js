f53e862de8efdc14920ae6a8c50d0d853e6a1ff5
Sean Chalmers <42610534+AQUASINE@users.noreply.github.com>
2025-03-14 04:01:16 -0500

    fix: deployment issues

--- frontend/src/helper/signin.js
+++ frontend/src/helper/signin.js
@@ -1,23 +1,21 @@
 import { UserManager, Log } from "oidc-client-ts";
 
 // re-enable if having issues with login
-// Log.setLogger(console);
-// Log.setLevel(Log.DEBUG);
+Log.setLogger(console);
+Log.setLevel(Log.DEBUG);
 
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_d46KXnFGn",
     client_id: "sep7sl8h18qssf0tc94p9pc3r",
-    redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+    redirect_uri: "https://d3rq77oh23742.cloudfront.net/index.html",
     response_type: "code",
     scope: "phone openid email"
 };
 
 if (import.meta.env.DEV) {
     cognitoAuthConfig.redirect_uri = "http://localhost:5173";
-} else {
-    // redirect to the website
-    cognitoAuthConfig.redirect_uri = window.location.origin;
 }
+console.log("redirect_uri: ", cognitoAuthConfig.redirect_uri);
 
 // create a UserManager instance
 export const userManager = new UserManager({
